b4ed77e092a6778d7fb70903893951f5913feb09
Cliff <tzcliff@outlook.com>
2019-11-06 17:38:28 -0500

    CRUD

--- BankATM/src/bank/MySql/DBManager.java
+++ BankATM/src/bank/MySql/DBManager.java
@@ -1,4 +1,8 @@
 package bank.MySql;
+
+import bank.CustomerAccount;
+
+import bank.*;
 import java.sql.*;
 public class DBManager {
     public void connect(){
@@ -13,5 +17,41 @@ public class DBManager {
             con.close();
         }catch(Exception e){ System.out.println(e);}
     }
+
+    public void addPerson(CustomerAccount customerAccount){
+
+    }
+    public void addCheckingAccount(CheckingAccount checkingAccount){
+
+    }
+    public void addSavingAccount(SavingsAccount savingsAccount){
+
+    }
+
+//    public void addSecurityAccount(SavingsAccount savingsAccount){
+//
+//    }
+
+    public void updatePerson(CustomerAccount customerAccount){
+
+    }
+    public void updateCheckingAccount(CheckingAccount checkingAccount){
+
+    }
+    public void updateSavingAccount(SavingsAccount savingsAccount){
+
+    }
+
+
+    public void deletePerson(CustomerAccount customerAccount){
+
+    }
+    public void deleteCheckingAccount(CheckingAccount checkingAccount){
+
+    }
+    public void deleteSavingAccount(SavingsAccount savingsAccount){
+
+    }
+
 }
 
