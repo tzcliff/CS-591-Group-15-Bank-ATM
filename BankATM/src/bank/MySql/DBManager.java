a35fa5ff2e6c950e98137bc45bfd43bfe54442f1
Cliff <tzcliff@outlook.com>
2019-11-09 17:41:43 -0500

    Update stocks

--- BankATM/src/bank/MySql/DBManager.java
+++ BankATM/src/bank/MySql/DBManager.java
@@ -3,6 +3,8 @@ package bank.MySql;
 import bank.CustomerAccount;
 
 import bank.*;
+
+import java.nio.charset.CharsetEncoder;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,11 +29,11 @@ public class DBManager {
     }
 
     public void addPerson(CustomerAccount customerAccount){
-        String sql = "INSERT INTO bank_atm.person (first_name, last_name) VALUES (\'"+ customerAccount.getPerson().getName().getFirstName() + "\', \'"+ customerAccount.getPerson().getName().getFirstName()+ "\');";
+        String sql = "INSERT INTO bank_atm.person (name) VALUES (\'"+ customerAccount.getPerson().getName().getFirstName() + " "+ customerAccount.getPerson().getName().getFirstName()+ "\');";
         sqlExecute(sql);
     }
     public void addCheckingAccount(CheckingAccount checkingAccount){
-        String sql = "INSERT INTO bank_atm.account (balance, routing_num, acc_num, active, open_fee, close_fee, transaction_fee, withdrrawal_fee, type) VALUES (\'" + checkingAccount.getBalanceInLocalCurrency() + "\', \'" + checkingAccount.getRoutingNumber() + "\', \'" + checkingAccount.getAccountNumber() + "\', " +
+        String sql = "INSERT INTO bank_atm.account (balance, routing_num, acc_num, active, open_fee, close_fee, transaction_fee, withdrrawal_fee, type, person_name) VALUES (\'" + checkingAccount.getBalanceInLocalCurrency() + "\', \'" + checkingAccount.getRoutingNumber() + "\', \'" + checkingAccount.getAccountNumber() + "\', " +
                 ""+ checkingAccount.isActive() +", \'"+ checkingAccount.getOpeningCharge() +"\', \'"+ checkingAccount.getClosingCharge() +"\', \'" + checkingAccount.getTransferFee() + "\', \'"+ checkingAccount.getWithdrawalFee() +"\', \'" + "C" +"\');";
         sqlExecute(sql);
     }
@@ -53,8 +55,15 @@ public class DBManager {
     }
 
     public void addLoan(Loan loan, CustomerAccount customerAccount){
-        String sql = " INSERT INTO bank_atm.loan (initial_amount, debt, person_name, interest) VALUES (\'"+loan.getInitialAmountInLocalCurrency()+"\', \'"+loan.getDebtInLocalCurrency()+"\', \'"+customerAccount.getPerson().getName().getFirstName()+"\', \'"+loan.getInterest()+"\');";
+        String sql = " INSERT INTO bank_atm.loan (initial_amount, debt, name, interest) VALUES (\'"+loan.getInitialAmountInLocalCurrency()+"\', \'"+loan.getDebtInLocalCurrency()+"\', \'"+customerAccount.getPerson().getName().getFirstName()+ " " +  customerAccount.getPerson().getName().getLastName()+"\', \'"+loan.getInterest()+"\');";
         sqlExecute(sql);
+    }
+
+    public void addStock(Stock stock){
+
+        String sql = "INSERT INTO bank_atm.stock (price, total_shares, avai_shares) VALUES (\' " + stock.getCurrentPrice()+ "\', \'"+ stock.getTotalShares()+"\', \'"+ stock.getCurrentlyAvailableShares()+"\');";
+        sqlExecute(sql);
+
     }
 
 
@@ -96,6 +105,45 @@ public class DBManager {
         catch(Exception e){ System.out.println(e);}
         return list;
     }
+
+    public void updateCheckingAccount(CheckingAccount checkingAccount){
+        String sql = "UPDATE bank_atm.account SET balance = \'" + checkingAccount.getBalanceInLocalCurrency() +  "\', active = "+ checkingAccount.isActive() + " , open_fee = \'" + checkingAccount.getOpeningCharge() +"\', close_fee = \'" + checkingAccount.getClosingCharge() +"\', transaction_fee = \'" + checkingAccount.getTransferFee() + "\', " +
+                "withdrrawal_fee = \'"+ checkingAccount.getWithdrawalFee() +"\' WHERE (acc_num = \' "+ checkingAccount.getAccountNumber()+" \' AND routing_num = \'"+ checkingAccount.getAccountNumber()+ " \');\n";
+        sqlExecute(sql);
+    }
+
+    public void updateSavingAccount(SavingsAccount savingsAccount){
+        String sql = "UPDATE bank_atm.account SET balance = \'" + savingsAccount.getBalanceInLocalCurrency() +  "\', active = "+ savingsAccount.isActive() + " , open_fee = \'" + savingsAccount.getOpeningCharge() +"\', close_fee = \'" + savingsAccount.getClosingCharge() +"\'," +
+                "interest = \'"+ savingsAccount.getInterest() +"\' WHERE (acc_num = \' "+ savingsAccount.getAccountNumber()+" \' AND routing_num = \'"+ savingsAccount.getAccountNumber()+ " \');\n";
+        sqlExecute(sql);
+    }
+
+    public void updateAccount(Account account){
+        String sql = "UPDATE bank_atm.account SET balance = \'" + account.getBalanceInLocalCurrency() +  "\', active = "+ account.isActive() + " , open_fee = \'" + account.getOpeningCharge() +"\', close_fee = \'" + account.getClosingCharge() +"\'," +
+                "\' WHERE (acc_num = \' "+ account.getAccountNumber()+" \' AND routing_num = \'"+ account.getAccountNumber()+ " \');\n";
+        sqlExecute(sql);
+    }
+
+    public void updateStock(Stock stock){
+        String sql = "UPDATE bank_atm.stock SET price = \'" +stock.getCurrentPrice()+ "\', total_shares = \'"+ stock.getTotalShares()+"\', avai_shares = \'"+stock.getCurrentlyAvailableShares()+"\', WHERE (name = \'"+stock.getName()+"\');";
+
+        sqlExecute(sql);
+    }
+
+    public void updateBoughtStock(BoughtStock boughtStock, SecurityAccount account){
+
+
+        String sql = "UPDATE bank_atm.bought_stock SET share_amount = \'"+ boughtStock.getAmountOfStocks() +"\', worth = \'" + boughtStock.getTotalAmountSpentOnBuying()+ "\' WHERE (stock_name = \'"+boughtStock.getStock().getName()+"\' AND account_id = \'" + account.getAccountNumber()+"\');";
+        sqlExecute(sql);
+
+    }
+
+    public void deleteBoughtStock(BoughtStock boughtStock, SecurityAccount account){
+
+    }
+
+
+
 
 
 
